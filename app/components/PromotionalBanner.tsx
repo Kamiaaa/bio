4b3a6756915a7fc152d6ae62b6c55df4429f6ffb
Mizan <mail.kazimizan@gmail.com>
2025-07-13 17:36:37 +0600

    Image link changed

--- app/components/PromotionalBanner.tsx
+++ app/components/PromotionalBanner.tsx
@@ -20,7 +20,7 @@ const PromotionalBanner = () => {
       ref={containerRef}
       className="relative w-full h-[500px] overflow-hidden bg-fixed bg-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: "url('/img/jolshiri.jpg')",
+        backgroundImage: "/img/jolshiri.jpg'",
       }}
     >
       <motion.div
